d237d221158ef15da80f12cac1ae2b2479ce93eb
Lynx3272 <tegoche21@gmail.com>
2026-09-13 18:36:06 +0300

    Remove plugin and dependency resolution management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,3 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-}
-
-rootProject.name = "KankiAnimeSistemi"
 include(":ExampleProvider")
+project(":ExampleProvider").projectDir = file("ExampleProvider")
+rootProject.name = "KankiAnimeSistemi"
